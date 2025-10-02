76994a8d7be0db945d7a798c247af5777283d3e9
Nicolas <gagnantnicolas64@gmail.com>
2025-10-02 16:26:55 +0200

    create Dockerfile and build docker

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/new_personnage')
+@app.route('/')
 def new_personnage():
     return render_template('new_personnage.html')
 
